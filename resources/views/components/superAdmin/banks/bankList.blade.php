2114cb1e41f8141ae618601eb4539edd2440cb9c
Dilan kanishka <dilankanishka032@gmail.com>
2024-07-30 13:14:07 +0530

    update bank list

--- resources/views/components/superAdmin/banks/bankList.blade.php
+++ resources/views/components/superAdmin/banks/bankList.blade.php
@@ -12,7 +12,7 @@
         </div>
     </div>
 
-    @foreach ($banks as $bank)
+    {{-- @foreach ($banks as $bank)
     <div class="p-3 mb-2 bg-primary-subtle text-primary-emphasis rounded">
         <div class="d-flex justify-content-between">
             <div>
@@ -27,6 +27,92 @@
         </div>
     </div>
     @endforeach
-</div>
+</div> --}}
     
 {{-- <div class="p-3 mb-2 bg-danger-subtle text-danger-emphasis rounded"> --}}
+
+    <div class="accordion accordion-flush" id="accordionFlushExample">
+        @if ($banks->isNotEmpty())
+            @foreach ($banks as $key => $bank)
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="flush-heading{{ $key }}">
+                        <button class="accordion-button collapsed d-flex justify-content-between align-items-center bg-primary-subtle text-primary-emphasis rounded" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse{{ $key }}" aria-expanded="false" aria-controls="flush-collapse{{ $key }}">
+                            <div class="w-100 d-flex justify-content-between align-items-center">
+                                <div>
+                                    <span class="badge text-bg-dark">{{ $bank->id }}</span>
+                                    <span class="fw-bold ms-2">{{ $bank->bank_name }}</span>
+                                </div> 
+                                <div>
+                                    {{ $bank->bank_address }}
+                                </div>
+                                <div></div>
+                            </div>
+                        </button>                       
+                    </h2>
+                    <div id="flush-collapse{{ $key }}" class="accordion-collapse collapse" aria-labelledby="flush-heading{{ $key }}" data-bs-parent="#accordionFlushExample">
+                        <div class="accordion-body ">
+                            <div class="massage-box-main border border-2 px-4 py-2">
+                                <div class="row">
+                                    <div class="col fw-bold">
+                                        Bank id : <br>
+                                        Bank registration done : <br>
+                                        Bank Name : <br>
+                                        Address : <br>
+                                        Email : <br>
+                                        Contact Number : <br>
+                                    </div>
+                                    <div class="col">
+                                        <b>{{ $bank->id }}</b> <br>
+                                        {{ \Carbon\Carbon::parse($bank->created_at)->format('d M,Y , h:i A') }} <br>
+                                        {{ $bank->bank_name }} <br>
+                                        {{ $bank->bank_address }} <br>
+                                        {{ $bank->email }} <br>
+                                        {{ $bank->bank_contact_num }} <br>
+                                    </div>
+                                </div>
+
+                                <hr>
+
+                                <div class="row">
+                                    <div class="col">
+                                        <span class="badge text-bg-dark p-2"> Administrators : </span> <br>
+                                        amal <br>
+                                        nimal <br>
+                                        
+                                    </div>
+                                    <div class="col">
+                                         <b>2</b> <br>
+                                        077 8844632 <br>
+                                        072 8411789 <br>
+                                    </div>
+                                </div>
+
+                                <hr>
+
+                                <div class="row">
+                                    <div class="col">
+                                        <span class="badge text-bg-dark p-2"> users : </span> <br>
+                                        amal <br>
+                                        nimal <br>
+                                        nimal <br>
+                                        nimal <br>
+                                        
+                                    </div>
+                                    <div class="col">
+                                        <b>10</b> <br>
+                                        077 8844632 <br>
+                                        072 8411789 <br>
+                                        072 8411789 <br>
+                                        072 8411789 <br>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                        <br>
+                    </div>
+                </div> <br>
+            @endforeach
+        @endif
+    </div>
+</div>
--- resources/views/superAdmin/users.blade.php
+++ resources/views/superAdmin/users.blade.php
@@ -1,4 +1,7 @@
 @extends('layouts.superAdminLayout')
 @section('SuperAdminContent')
+
     <h1>users</h1>
+    <hr class="me-3">
+    
 @endsection
