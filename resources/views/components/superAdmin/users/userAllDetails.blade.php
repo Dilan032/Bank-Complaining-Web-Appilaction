5ecd834657b2ff82c9b00879d0da49e28501a508
Dilan kanishka <dilankanishka032@gmail.com>
2024-08-02 15:19:12 +0530

    update users all details

--- resources/views/components/superAdmin/users/userAllDetails.blade.php
+++ resources/views/components/superAdmin/users/userAllDetails.blade.php
@@ -2,17 +2,17 @@
     <div class="col-md-5">
         <div class="bg-body-secondary border border-3 rounded p-3 m-5 mb-2">
             <span class="fs-4">Administrators</span> <br>
-            All - [ {{$activeAdministratorCount + $inactiveAdministratorCount}} ] <br>
-            Active  - [ {{$activeAdministratorCount}} ] <br>
-            Inactive  - [ {{$inactiveAdministratorCount}} ] <br> 
+            All <span class="badge text-bg-light fs-6"> {{$activeAdministratorCount + $inactiveAdministratorCount}} </span> <br>
+            Active <span class="badge text-bg-light fs-6"> {{$activeAdministratorCount}} </span> <br>
+            Inactive <span class="badge text-bg-light fs-6"> {{$inactiveAdministratorCount}} </span> <br> 
         </div>
     </div>
     <div class="col-md-5">
         <div class="bg-body-secondary border border-3 rounded p-3 m-5 mb-2">
             <span class="fs-4">Employees</span> <br>
-            All  [ {{$activeUserCount + $inactiveUserCount}} ] <br> 
-            Active  [ {{$activeUserCount}} ] <br> 
-            Inactive  [ {{$inactiveUserCount}} ] <br> 
+            All <span class="badge text-bg-light fs-6"> {{$activeUserCount + $inactiveUserCount}} </span> <br> 
+            Active <span class="badge text-bg-light fs-6"> {{$activeUserCount}} </span> <br> 
+            Inactive <span class="badge text-bg-light fs-6"> {{$inactiveUserCount}} </span> <br> 
         </div>
     </div>
 </div>
