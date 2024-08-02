fd7c131b7b59d324df05ef030132ce0e1cb58905
Dilan kanishka <dilankanishka032@gmail.com>
2024-08-02 15:19:48 +0530

    update banks all details

--- resources/views/components/superAdmin/banks/bankAllDetails.blade.php
+++ resources/views/components/superAdmin/banks/bankAllDetails.blade.php
@@ -2,19 +2,19 @@
     <div class="col-md-5">
         <div class="bg-body-secondary border border-3 rounded p-3 m-5 mb-2">
             <span class="fs-4">Banks</span> <br>
-            All - <span class="badge text-bg-light">{{$bankCount}}</span>  <br>
-            Active  - <span class="badge text-bg-light">{{$activeBankCount}}</span>  <br>
-            Inactive  - <span class="badge text-bg-light">{{$inactiveBankCount}} </span> <br> 
+            All - <span class="badge text-bg-light fs-6">{{$bankCount}}</span>  <br>
+            Active  - <span class="badge text-bg-light fs-6">{{$activeBankCount}}</span>  <br>
+            Inactive  - <span class="badge text-bg-light fs-6">{{$inactiveBankCount}} </span> <br> 
         </div>
     </div>
     <div class="col-md-5">
         <div class="bg-body-secondary border border-3 rounded p-3 m-5 mb-2">
             <span class="fs-4">Problem</span> <br>
-            All <span class="badge text-bg-light"> yy </span>
+            All <span class="badge text-bg-light fs-6"> yy </span>
             <br> 
-            solved <span class="badge text-bg-light"> yy </span>
+            solved <span class="badge text-bg-light fs-6"> yy </span>
              <br> 
-            should be resolved <span class="badge text-bg-light"> yy </span>
+            should be resolved <span class="badge text-bg-light fs-6"> yy </span>
              <br>  
         </div>
     </div>
