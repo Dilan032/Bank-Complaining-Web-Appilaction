598444cf2ff1fafe6e4732515f36900f6e459204
Dilan kanishka <dilankanishka032@gmail.com>
2024-08-02 15:01:17 +0530

    update users all details

--- resources/views/components/superAdmin/users/userAllDetails.blade.php
+++ resources/views/components/superAdmin/users/userAllDetails.blade.php
@@ -1,27 +1,30 @@
-<div class="d-flex justify-content-evenly mt-5 mb-5 text-center px-auto">
-    <div class="p-3 bg-primary bg-opacity-10 border border-primary">
-        All Administrators <br> [ {{$activeAdministratorCount + $inactiveAdministratorCount}} ]
+{{-- <div class="d-flex justify-content-evenly mt-5 mb-5 text-center px-auto">
+    <div class="p-3 bg-primary bg-opacity-10 border border-primary text-start">
+             
     </div>
-    <div class="p-3 bg-primary bg-opacity-10 border border-primary">
-        Active Administrators <br> 
-        [ {{$activeAdministratorCount}} ]
+
+    <div class="px-5 bg-primary bg-opacity-10 border border-primary text-start">
+           
     </div>
-    <div class="p-3 bg-danger bg-opacity-10 border border-danger">
-        Inactive Administrators <br>
-       [ {{$inactiveAdministratorCount}} ]
+
+</div> --}}
+
+
+<div class="row mb-5 d-flex justify-content-center"">
+    <div class="col-md-5">
+        <div class="bg-body-secondary border border-3 rounded p-3 m-5 mb-2">
+            <span class="fs-4">Administrators</span> <br>
+            All - [ {{$activeAdministratorCount + $inactiveAdministratorCount}} ] <br>
+            Active  - [ {{$activeAdministratorCount}} ] <br>
+            Inactive  - [ {{$inactiveAdministratorCount}} ] <br> 
+        </div>
     </div>
-    <div class="p-3 bg-primary bg-opacity-10 border border-primary">
-        All Employees <br> 
-        [ {{$activeUserCount + $inactiveUserCount}} ]
-    </div>
-    <div class="p-3 bg-primary bg-opacity-10 border border-primary">
-        Active Employees <br> 
-        [ {{$activeUserCount}} ]
-    </div>
-    <div class="p-3 bg-danger bg-opacity-10 border border-danger">
-        Inactive Employees <br> 
-        [ {{$inactiveUserCount}} ]
+    <div class="col-md-5">
+        <div class="bg-body-secondary border border-3 rounded p-3 m-5 mb-2">
+            <span class="fs-4">Employees</span> <br>
+            All  [ {{$activeUserCount + $inactiveUserCount}} ] <br> 
+            Active  [ {{$activeUserCount}} ] <br> 
+            Inactive  [ {{$inactiveUserCount}} ] <br> 
+        </div>
     </div>
 </div>
-
-
