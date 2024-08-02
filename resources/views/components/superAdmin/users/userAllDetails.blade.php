59476bbf6383083bff7ea01a9dc0024a6edb75cd
Dilan kanishka <dilankanishka032@gmail.com>
2024-08-02 15:01:39 +0530

    update users all details

--- resources/views/components/superAdmin/users/userAllDetails.blade.php
+++ resources/views/components/superAdmin/users/userAllDetails.blade.php
@@ -1,15 +1,3 @@
-{{-- <div class="d-flex justify-content-evenly mt-5 mb-5 text-center px-auto">
-    <div class="p-3 bg-primary bg-opacity-10 border border-primary text-start">
-             
-    </div>
-
-    <div class="px-5 bg-primary bg-opacity-10 border border-primary text-start">
-           
-    </div>
-
-</div> --}}
-
-
 <div class="row mb-5 d-flex justify-content-center"">
     <div class="col-md-5">
         <div class="bg-body-secondary border border-3 rounded p-3 m-5 mb-2">
