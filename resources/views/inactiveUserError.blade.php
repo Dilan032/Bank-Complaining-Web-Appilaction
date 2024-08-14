57ad0636053a0bcb9b7b8668aa59cce99f676bfb
Dilan kanishka <dilankanishka032@gmail.com>
2024-08-15 03:36:07 +0530

    update inactive user interface

--- resources/views/inactiveUserError.blade.php
+++ resources/views/inactiveUserError.blade.php
@@ -9,10 +9,13 @@
   <body>
 
     {{-- <h1>Account is inactive</h1> --}}
-    <p class="font-monospace text-center mt-5">
-        Your account has been temporarily suspended <br>
-        Contact your bank manager
-    </p>
+    <div class="alert alert-warning" role="alert">
+        <p class="font-monospace text-center my-5">
+            Your account has been temporarily suspended <br>
+            Contact your bank manager
+        </p>
+    </div>
+    
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
