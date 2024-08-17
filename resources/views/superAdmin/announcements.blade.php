bb443771aedc709bf47baa8325566b37e6b18ecb
Dilan kanishka <dilankanishka032@gmail.com>
2024-08-17 22:28:49 +0530

    update super admin page user and bank pages UI

--- resources/views/superAdmin/announcements.blade.php
+++ resources/views/superAdmin/announcements.blade.php
@@ -21,24 +21,24 @@
 
     <div class="row justify-content-center mt-5">
         <div class="col-md-10">
-            <section  class="bg-white text-dark rounded p-3 messageBG">
-            <h3 class="mb-5 text-center fw-normal">Write a message to send <small class="font-monospace">(via Email) </small></h3>
+            <section  class="bg-primary-subtle text-dark rounded p-3 messageBG">
+            <h3 class="mb-5 text-center fw-bold">Write a message to send <small class="font-monospace">(via Email) </small></h3>
                 <form action="" method="post" enctype="multipart/form-data">
                     @csrf
                     
                     <input type="hidden" name="bank_id" value="">
 
-                    <select class="form-select mb-3" aria-label="Default select example">
+                    <select class="form-select mb-3 messageBG" aria-label="Default select example">
                         <option value="1">To : All Bank Employees</option>
                         <option value="2">To : All Bank Administrators</option>
                     </select>
 
-                    <div class="form-floating mb-3">
+                    <div class="form-floating mb-3 messageBG">
                         <input type="text" name="subject" value="{{old('subject')}}" class="form-control" id="floatingInput" placeholder="Subject">
                         <label for="floatingInput">Subject</label>
                     </div>
                     <div class="form-floating">
-                        <textarea class="form-control" name="message" value="{{old('message')}}" placeholder="Message" id="floatingTextarea2" style="height: 250px"></textarea>
+                        <textarea class="form-control messageBG" name="message" value="{{old('message')}}" placeholder="Message" id="floatingTextarea2" style="height: 250px"></textarea>
                         <label for="floatingTextarea2">Message</label>
                     </div>
             
