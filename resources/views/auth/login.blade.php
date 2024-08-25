96f546edb48a1fd0a2662469e263cb4e20a088dc
Dilan kanishka <dilankanishka032@gmail.com>
2024-08-25 16:59:32 +0530

    develop login form sweet alert

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,52 +1,3 @@
-{{-- <x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-
- 
-        <div>
-            <x-input-label for="login" :value="__('UserName /  Email')" />
-            <x-text-input id="login" class="block mt-1 w-full" type="text" name="login" :value="old('login')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-      
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout> --}}
-
-
 <!doctype html>
 <html lang="en">
   <head>
@@ -71,29 +22,37 @@
         }
 
         </style>
+
+    {{-- for sweetalert --}}
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
         
 </head>
 
 <body class="bg-light-subtle">
     <div class="container" >
-<div id="particles-js">
+        <div id="particles-js">
+
+        <!-- Display validation errors -->
+        @if ($errors->any())
+            @foreach ($errors->all() as $error)
+                <script>
+                    Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: "{{ $error }}",
+                    });
+                </script>
+            @endforeach
+        @endif
 
         <img class="p-2" src="{{ asset('images/CompanyLogo/nanosoftSolutions Company Logo.png') }}" alt="NanosoftSolutions Logo">
         
-        <h2 class="text-center mt-1">Bank Complaning Web Application</h2>
+        <h1 class="text-center mt-1">Welcome to </h1>
 
         <div class="row justify-content-center">
-            <div class="col-md-6 mt-5">
+            <div class="col-md-6 mt-3">
                 <div class="border p-4 bg-white text-dark rounded shadoLoginBG">
-                    <h2 class="text-center mb-4 fw-medium">Login Form</h2>
-                    <!-- Display validation errors -->
-                    @if ($errors->any())                
-                        @foreach ($errors->all() as $error)
-                            <div class="alert alert-danger">
-                                {{ $error }}
-                            </div>
-                        @endforeach
-                    @endif 
+                    <h4 class="text-center mb-4 fw-medium">Bank Complaint Web Application</h4>
                     <form action="{{route('login')}}" method="POST" class="mx-auto">
                         @csrf
                         <div class="form-floating mb-3">
