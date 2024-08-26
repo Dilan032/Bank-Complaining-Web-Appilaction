4a530706f9174818043631137f3c1ab3f7e99dc4
Dilan kanishka <dilankanishka032@gmail.com>
2024-08-26 09:33:10 +0530

    develop responcive side menu for super admin

--- resources/views/components/superAdmin/sideMenu.blade.php
+++ resources/views/components/superAdmin/sideMenu.blade.php
@@ -24,12 +24,6 @@
                         <span class="d-none d-sm-inline">Messages</span>
                     </a>
                 </li>
-                {{-- <li class="nav-item mb-2">
-                    <a href="{{route('superAdmin.announcements.view')}}" class="nav-link text-white">
-                        <i class="bi bi-megaphone me-2 ms-2"></i>
-                        <span class="d-none d-sm-inline">Announcements</span>
-                    </a>
-                </li> --}}
                 <li class="nav-item mb-2">
                     <a href="{{route('superAdmin.users.view')}}" class="nav-link text-white">
                         <i class="bi bi-person-circle me-2 ms-2"></i>
@@ -57,4 +51,61 @@
 
         </div>
     </div>
+</div>
+
+
+{{-- in mobile view show this side bar --}}
+<i class="bi bi-list fs-1 ms-4 mt-2" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling"></i>
+
+<div class="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+  <div class="offcanvas-header">
+    <h4 class="offcanvas-title" id="offcanvasScrollingLabel">SuperAdmin</h4>
+    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+    
+    <h6>Bank Complaning Web Application</h6>
+
+    <hr class="text-dark">
+    
+    <ul class="nav nav-pills flex-column mb-auto p-2">
+        <li class="nav-item mb-2">
+            <a href="{{ route('superAdmin.dashbord') }}" class="nav-link text-dark ">
+                <i class="bi bi-speedometer"></i>
+                <span>Dashbord</span>
+            </a>
+        </li>
+        <li class="nav-item mb-2">
+            <a href="{{ route('superAdmin.messages.view') }}" class="nav-link text-dark">
+                <i class="bi bi-chat-left-dots"></i>
+                <span>Messages</span>
+            </a>
+        </li>
+        <li class="nav-item mb-2">
+            <a href="{{ route('superAdmin.users.view') }}" class="nav-link text-dark">
+                <i class="bi bi-person-circle"></i>
+                <span>Users</span>
+            </a>
+        </li>
+        <li class="nav-item mb-2">
+            <a href="{{ route('superAdmin.banks.view') }}" class="nav-link text-dark">
+                <i class="bi bi-person-circle"></i>
+                <span>Banks</span>
+            </a>
+        </li>
+    </ul>
+
+    <hr class="text-dark">
+
+    <ul class="nav nav-pills flex-column mb-auto">
+        <li class="nav-item">
+            <a href="{{route('administrator.logout')}}" class="nav-link text-dark">
+                <i class="bi bi-box-arrow-right"></i>
+                <span>LogOut</span>
+            </a>
+        </li>
+    </ul>
+
+
+  </div>
 </div>
